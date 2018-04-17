2caa5256757c6c9d7f5bef1a28869542b16bc05b
bios90 <bios90@mail.ru>
2018-04-18 00:00:48 +0300

    my test commit 2

--- app/src/main/java/com/example/bios90/teatapp/testClass.java
+++ app/src/main/java/com/example/bios90/teatapp/testClass.java
@@ -1,4 +1,17 @@
 package com.example.bios90.teatapp;
 
-public class testClass {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class testClass extends AppCompatActivity{
+
+    int j = 27;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        j=j*j;
+    }
 }
